a6f440d614aa3bed40902c12a9109795f0bb714d
Josh <josh@macawls.dev>
2024-05-06 12:42:14 +0200

    feat(models): add borrow record model

---
+++ Models/BorrowRecord.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LibraryService.Models;
+
+[Table("borrow_record")]
+[JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+public class BorrowRecord : BaseModel
+{
+    [PrimaryKey("id")]
+    public int Id { get; set; }
+    
+    [Column("created_at")]
+    [SwaggerSchema(ReadOnly = true)]
+    public DateTime CreatedAt { get; set; }
+    
+    [Column("due_date")]
+    public DateTime DueDate { get; set; }
+    
+    [Column("book_instance_id")]
+    [JsonProperty("book_instance_id")]
+    public int BookInstanceId { get; set; }
+    
+    [Column("member_id")]
+    [JsonProperty("member_id")]
+    public int MemberId { get; set; }
+    
+    public static BorrowRecord Create(DateTime dueDate, int bookInstanceId, int memberId)
+    {
+        return new BorrowRecord
+        {
+            BookInstanceId = bookInstanceId,
+            MemberId = memberId
+        };
+    }
+}
---
+++ Repositories/Concrete/BorrowRecord/InMemoryBorrowRecordRepository.cs
@@ -0,0 +1,21 @@
+﻿using LibraryService.Models;
+
+namespace LibraryService.Repositories;
+
+public class InMemoryBorrowRecordRepository(string filePath) : InMemoryRepositoryBase<BorrowRecord>(filePath)
+{
+    public override Task Update(BorrowRecord item)
+    {
+        ArgumentNullException.ThrowIfNull(item);
+
+        var existingRecord = Items.FirstOrDefault(e => e.Id == item.Id);
+
+        if (existingRecord == null)
+        {
+            throw new InvalidOperationException($"Borrow Record with ID {item.Id} not found.");
+        }
+
+        existingRecord.DueDate = item.DueDate;
+        return Task.CompletedTask;
+    }
+}
